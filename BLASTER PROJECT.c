bb94ef4b8daff398c9ee3cc90f9d0b34a7740acc
Maltob03 <46380730+Maltob03@users.noreply.github.com>
2022-05-27 00:07:16 +0200

    first commit

---
+++ BLASTER PROJECT.c
@@ -0,0 +1,135 @@
+///Conversione sequenza genica
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 1000000
+
+
+char * matrix[2][64]= 
+    { 
+        {"UUU","UUC","UUA","UUG","CUU","CUC","CUA","CUG","AUU","AUC","AUA","AUG","GUU","GUC","GUA","GUG","UCU","UCC","UCA","UCG","CCU","CCC","CCA","CCG","ACU","ACC","ACA","ACG","GCU","GCC","GCA","GCG","UAU","UAC","UAA","UAG","CAU","CAC","CAA","CAG","AAU","AAC","AAA","AAG","GAU","GAC","GAA","GAG","UGU","UGC","UGA","UGG","CGU","CGC","CGA","CGG","AGU","AGC","AGA","AGG","GGU","GC","GGA","GGG"},
+        {"F","F","L","L","L","L","L","L","I","I","I","M","V","V","V","V","S","S","S","S","P","P","P","P","T","T","T","T","A","A","A","A","Y","Y"," STOP "," STOP ","H","H","Q","Q","N","N","K","K","D","D","E","E","C","C"," STOP ","W","R","R","R","R","S","S","R","R","G","G","G","G"}
+    };
+
+void DNA_to_mRNA (int lenght, char gene[]);
+int conversione(int lenght, char gene[]);
+int stop(int lenght, char gene[],int c);
+void life_function (int start,int stop,char gene[]);
+int main ()
+
+{
+    long long int lenght;
+    long long int conversion;
+    long long int ferma;
+
+    printf("BENVENUTO DOTT/DOTT.SSA \n Per effetturare l'analisi della sequenza genica va modificato il file prova.txt inserendo al suo interno solo ed esclusivamente la seqeuenza genica \n Al termine dell'analisi sarà stampata la sequenza amminoacidica \n Buon Lavoro \n" );
+    
+    
+   
+    char gene[MAX];
+    FILE *fptr;
+    if ((fptr = fopen("prova.txt", "r")) == NULL) {
+        printf("Error! File cannot be opened.");
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+
+    // reads text until newline is encountered
+    fscanf(fptr, "%[^\n]", gene);
+    fclose(fptr);
+
+
+    lenght = strlen(gene);
+    //printf("%s\n", "inserisci stringa di un gene da convertire in mRNA");
+        
+    DNA_to_mRNA(lenght,gene);
+    printf("%s\n%s\n","Sequenza di mRNA", gene);  
+    conversion = conversione(lenght,gene);   
+    ferma = stop(lenght,gene,conversion);   
+    life_function(conversion,ferma,gene);
+
+    printf(" \nPuoi ricavare la proteina corrispondente usando la sequenza amminoacidica usando SMART BLAST al seguente link :https://blast.ncbi.nlm.nih.gov/smartblast/?LINK_LOC=BlastHomeLink");
+    
+
+}
+
+
+
+
+
+
+void DNA_to_mRNA (int lenght, char gene[])
+{
+    for (int i=0; i<=lenght; i++)
+    {
+        if( gene[i] == 'T' )
+        {
+            gene[i] = 'U';
+            
+        }
+        
+    }
+}
+
+
+int conversione(int lenght, char gene[])
+{
+    int p_cstart;
+    int c_start=0;
+    int i=0;
+    while (c_start == 0)
+    {
+        if(gene[i]== 'A' && gene[i+1] == 'U' && gene[i+2]=='G')
+        {
+            printf("%s%d\n", "Trovato codone di start in posizione ", i);
+            
+            p_cstart = i;
+            c_start = 1;
+            break;
+        
+        }
+        i=i+3;
+    }
+    return p_cstart;
+}
+
+
+
+int stop(int lenght, char gene[], int c)
+{
+    int p_cstop;
+    for(c; c<=lenght; c=c+3)
+    {
+        if(gene[c]== 'U' && gene[c+1] == 'G' && gene[c+2]=='A' || gene[c]== 'U' && gene[c+1] == 'A' && gene[c+2]=='A' || gene[c]== 'U' && gene[c+1] == 'A' && gene[c+2]=='G')
+        {
+            printf("%s%d\n", "Trovato codone di stop in posizione ", c);
+            printf("%s\n","Incomincio la conversione:");
+            p_cstop = c;
+            break;
+        
+        }
+    }
+    return p_cstop;
+}
+
+
+void life_function (int start,int stop,char gene[])
+{
+    char a[MAX];
+    int x;
+    for(start;start<=stop;start=start+3)
+    {
+        a[0] = gene[start];
+        a[1]= gene[start+1];
+        a[2]=gene[start+2];
+        //printf("%s\n",a);
+        for(int k=0;k<64;k++)
+        {
+            x=strcmp(a,matrix[0][k]);
+            if(x==0)
+            {
+            printf("%s",matrix[1][k]);
+            }
+        }
+    }
+}
---
+++ DOCUMENTAZIONE PROGETTO.md
@@ -0,0 +1,57 @@
+Documentazione 
+
+Blaster DNA
+
+
+
+Questo script scritto in C permette di ottenere una sequenza di amminoacidi a partire da una sequenza di un particolare gene. 
+
+
+
+Questo processo viene eseguito in tutte le cellule in maniera quasi algoritmica. Una sequenza di DNA è costituita da 4 basi azotate A,C,G,T ovvero (adenina,citosina,guanina e timina). Questa sequenza in una cellula viene convertita in RNA spezzando la catena di DNA e usandola come una sorta di calco. E' bene ricordarsi che l'RNA sostituisce la base azotata T con U (uracile).
+
+
+
+A partire dalla sequenza di mRNA appena convertita all'interno della cellula avviene il procedimento più importante per lo sviluppo biologico ovvero la traduzione dell'mRNA in sequenza amminoacidica.
+
+La traduzione segue delle regole specifiche:
+
+-Solo alcune porzioni della sequenza di mRNA codificano un amminoacido, altre invece possono essere considerati placeholder, vengono chiamati "esoni"
+
+-La traduzione avviene prendendo in esame una sequenza di tre basi , tale sequenza viene chiamata"codone". Ogni codone codifica uno specifico amminoacido.
+
+-La polimerizzazione degli amminoacidi (il processo di traduzione) è un processo non ambiguo ma ridondante in quanto piu codoni possono codificare lo stesso amminoacido
+
+-Essendo 4 le basi azotate e i codoni formati da triplette di basi azotate le combinazioni possibili sono esattamente 4^3 ovvero 64 possibili combinazioni
+
+-Una volta ottenuta la sequenza amminoacidica si puo effettuare il cosi detto processo di balsting in modo da risalire alla proteina corrispondente e quindi alla sua funzione specifica all'interno dell'organismo
+
+
+
+##Funzionamento del programma
+
+Per effettuare lo studio della sequenza genica presa in esame è necessario caricare la stessa in un file .txt nella stessa cartella del file sorgente in modo da poterla caricare e sottoporre all'algoritmo
+
+Una volta caricato il file l'algoritmo appena eseguito stamperà la sequenza genica di input eseguirà la traduzione e verrà stampata a video
+
+
+
+TO-DO
+
+Trasformare in funzione la parte di codice che permette il caricamento della sequenza da file esterno
+
+
+
+Aggiungere una funzione che permette di confrontare due sequenze geniche di organismi diversi, come ad esempio un gene che codifica la stessa proteina negli esseri umani e in animali e tramite comparazione stabilire che rapporto c'è dal punto di vista di discendenza e di mutazioni 
+
+
+
+-Sfruttare le librerie del database NCBI per effettuare il processo di BLAST all'interno dello script (o almeno aprire il browser e fare la ricerca in automatico)
+
+
+
+I dati sono presi dal database NCBI (National Center for Biotechnology Information) , piattaforma dedicata interamente alla raccolta di dati biologici
+
+
+
+
